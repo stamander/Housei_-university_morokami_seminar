dd677db8b1997f72d750209c9c3d6dfed1173258
stamander <iida.atsushi.fh@gmail.com>
2020-04-16 13:15:12 +0900

    Update 20200415013233_create_interviews.rb

--- db/migrate/20200415013233_create_interviews.rb
+++ db/migrate/20200415013233_create_interviews.rb
@@ -2,7 +2,7 @@ class CreateInterviews < ActiveRecord::Migration[5.2]
   def change
     create_table :interviews do |t|
       t.string :name,null: false
-      t.datetime :start_time
+      t.datetime :start_time,null: false
 
       t.timestamps
     end
